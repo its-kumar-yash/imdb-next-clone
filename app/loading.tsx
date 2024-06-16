37a3cd62732dfe6b171921f18cd202423f41c536
its-kumar-yash <its.yash.kumar23@gmail.com>
2024-06-16 15:04:52 +0530

    updated spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Loading = () => {
     return (
         <div>
-            <Image src="/loading.gif" alt="Loading" width={50} height={50} />
+            <Image src="/spinner.svg" alt="Loading" width={50} height={50} />
         </div>
     );
 }
